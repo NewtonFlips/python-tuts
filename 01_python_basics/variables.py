e5d42241d36d0fcb7e5ec9a33de58bdc7c396dae
Newton Flips <newtonflips@gmail.com>
2021-06-06 17:11:38 +0400

    python basics.....just printing to console

---
+++ 01_python_basics/variables.py
@@ -0,0 +1,7 @@
+# Variables are used to store different chunk values which can be used and reused and manipulated in various parts of a program. They also allow us to know what a particulat computation in a program is doing.
+
+day_hours = 24
+week_days = 7
+
+week_hours = day_hours * week_days
+print(week_hours)
